d94bc1486a8c87faeaebb41162e365da00ab7e88
Akshay Benny <akshay@mx-zenbook>
2023-10-12 02:21:04 +0530

    edit

--- embed.py
+++ embed.py
@@ -2,8 +2,9 @@ from PIL import Image
 
 
 def string_to_binary():
-    with open("message.txt", "r") as f:
-        secret_message = f.read()
+
+    f = open("message.txt", "r")
+    secret_message = str(f.read())
 
     # Convert string to Binary
     binary_representation = []
@@ -21,7 +22,8 @@ def embed_message_in_image(image_path, message_in_binary):
     img_rgb = img.convert('RGB')
     pixels = list(img_rgb.getdata())
 
-    modified_pixels = []
+    # For storing the modified pixels
+    new_pixels = []
 
     # Embed the message in the blue channel of the image
     message_index = 0
@@ -30,19 +32,22 @@ def embed_message_in_image(image_path, message_in_binary):
 
         # If there are still bits left to embed
         if message_index < len(message_in_binary):
+            # Get the next bit from the message
             bit = int(message_in_binary[message_index])
 
+            # Embed the bit in the blue channel
             if bit == 1 and b % 2 == 0:
                 b += 1
             elif bit == 0 and b % 2 == 1:
                 b -= 1
 
+            # Move to the next bit
             message_index += 1
 
-        modified_pixels.append((r, g, b))
+        new_pixels.append((r, g, b))
 
-    # Create new image with embeded data
-    img_rgb.putdata(modified_pixels)
+    # Update the image with the new pixels
+    img_rgb.putdata(new_pixels)
     img_rgb.save('./static/images/stego_image.bmp')
 
     return img_rgb
