cab612a7ed8c0b39cc5642a3e6c05850c2ac6e80
Akshay Benny <akshay@mx-zenbook>
2023-10-27 20:51:32 +0530

    successfull

--- v2/decode.py
+++ v2/decode.py
@@ -1,32 +1,27 @@
 from PIL import Image
 
 
-def decode():
+def text_decoder():
+    print("Image decoding started...")
     img_rgb = Image.open("./static/stego_image.bmp")
     pixels = list(img_rgb.getdata())
     binary_string = ''
     text_string = ''
-
+    print("Extracting pixels...")
     for pixel in pixels:
         r, g, b = pixel
         blue_binary = format(b, '08b')
         least_bit = blue_binary[-1]
         binary_string += least_bit
-
-    # Padding to ensure the length is a multiple of 8
+    print("Extraction complete!")
+    print("Taking data from pixels...")
     padding = 8 - len(binary_string) % 8
     binary_string = binary_string + '0' * padding
-
-    # Split binary string into groups of 8 bits
     groups_of_eight = [binary_string[i:i+8]
                        for i in range(0, len(binary_string), 8)]
-    print(groups_of_eight[0:100])
-    for byte in groups_of_eight:
-        decimal = int(byte, 2)
-        text = chr(decimal)
-        text_string += text
 
-    # print("Decoded text (in groups of eight bits):", text_string)
+    for byte in groups_of_eight[0:15]:
+        print(type(chr(int(byte, 2))))
+        text_string += chr(int(byte, 2))
 
-
-decode()
+    print(f"Decoded text from the image: {text_string}")
--- v2/encode.py
+++ v2/encode.py
@@ -1,16 +1,19 @@
 from PIL import Image
 
 
-def encode():
+def text_encoder():
     secret_text = str(input("Enter secret text:"))
     binary_data = text_to_binary(secret_text)
     embed_binary_in_image("./static/skyrim.bmp", binary_data)
+    print("Embedding completed successfully!")
 
 
 def text_to_binary(text):
+    print("Converting text to binary...")
     binary_list = []
     for char in text:
         binary_list.append(format(ord(char), '08b'))
+    print("Converted text to binary!")
     return {
         "binary": "".join(binary_list),
         "binary_length": len("".join(binary_list))
@@ -18,6 +21,7 @@ def text_to_binary(text):
 
 
 def embed_binary_in_image(image_path, binary_data):
+    print("Embedding binary data into the image...")
     img_rgb = Image.open(image_path).convert("RGB")
     pixels = list(img_rgb.getdata())
     new_pixels = []
@@ -29,12 +33,12 @@ def embed_binary_in_image(image_path, binary_data):
         else:
             channel_binary = bin(b)[2:]
             print(channel_binary)
-            new_pixels.append((r, g, int(
-                channel_binary[:-1] + binary_data["binary"][binary_message_read_index])))
+            new_pixel = (r, g, int(int(
+                channel_binary[:-1] + binary_data["binary"][binary_message_read_index], 2)))
+            new_pixels.append(new_pixel)
+            print(
+                f"Appended {channel_binary} into the new pixels ->>> {new_pixel}")
         binary_message_read_index += 1
 
     img_rgb.putdata(new_pixels)
     img_rgb.save('./static/stego_image.bmp')
-
-
-encode()
